6c7c75ccf9bab2304ae82fdfe2b5a7950354ff20
B09S104est <B09S104est@MEDAULAS.UPB>
2026-07-28 11:15:29 -0500

    Primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Agustin";
+            Console.Write("Hola " + nombre);
+        }
+    }
+}
