b94373e6ba128f9164076427667f3949bf10e238
agus <agus.b1215@protonmail.com>
2026-08-11 11:26:22 -0500

    Condicionales Anidados 11/08/26

---
+++ 5.CondicionalesAnidados/5.CondicionalesAnidados/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _5.CondicionalesAnidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diseñar un algoritmo que lea tres numeros A, B, C y visualice en pantalla el valor mas grande. El usuario debe ingresar tres valores diferentes
+
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            Console.WriteLine("Ingrese tres números diferentes");
+            Console.WriteLine("Número 1: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Número 2: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Número 3: ");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine(num1 + " es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine(num3 + " es el mayor");
+                }
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine(num2 + " es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine(num3 + " es el mayor");
+                }
+                     
+            }
+        }
+    }
+}
