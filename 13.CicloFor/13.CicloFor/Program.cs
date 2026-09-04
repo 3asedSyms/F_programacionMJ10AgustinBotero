f8057cd5a751a9d31a8f679de9d2ee367b2bd7b2
3asedSyms <agus.b1215@protonmail.com>
2026-09-03 20:24:10 -0500

    ejercicio casa ciclos for

--- 13.CicloFor/13.CicloFor/Program.cs
+++ 13.CicloFor/13.CicloFor/Program.cs
@@ -5,13 +5,31 @@ namespace _13.CicloFor
     {
         static void Main(string[] args)
         {
-            int acumulador = 0;
+            /*int acumulador = 0;
 
             for (int contador = 1; contador <= 5; contador++)
             {
                 acumulador += contador;
             }
-            Console.WriteLine("La suma de los primeros cinco números enteros es: " + acumulador);
+            Console.WriteLine("La suma de los primeros cinco números enteros es: " + acumulador);*/
+
+
+            //Realizar un programa que permita mostrar por pantalla los números múltiplos de 5,
+            //el usuario debe ingresar por el teclado el rango de números a evaluar.
+
+            int n;
+            int acumulador = 0;
+
+            Console.WriteLine("Ingrese la cantidad de multiplos de 5 a evaluar: ");
+            n = int.Parse(Console.ReadLine());
+
+            
+                for (int contador = 5; n > acumulador; contador += 5)
+                {
+                    acumulador++;
+                    Console.WriteLine(contador);
+                }
+            
         }
     }
 }
