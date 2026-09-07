f32aa4766293a00db1c10068d9f396701ea36d8e
3asedSyms <agus.b1215@protonmail.com>
2026-09-07 01:07:01 -0500

    Parcial condicionales resuelto

--- ParcialCondicionales/ParcialCondicionales/Program.cs
+++ ParcialCondicionales/ParcialCondicionales/Program.cs
@@ -9,43 +9,91 @@ namespace ParcialCondicionales
             
             string nombre;
             int añosTrabajo;
-            float añoActual = 0.0f;
-            float añoAnterior = 0.0f;
-
-            Console.WriteLine("Ingrese su nombre");
+            double añoActual;
+            double añoAnterior;
+            double promedio;
+            const int sueldoBase = 2500000;
+            double valorAumento;
+            
+            Console.WriteLine("Ingrese su nombre"); 
             nombre = Console.ReadLine();
             Console.WriteLine("Ingrese el numero de años trabajados en la empresa");
             añosTrabajo = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ingrese calificacion del año actual");
-            añoActual = Single.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese calificacion del año anterior");
-            añoAnterior = Single.Parse(Console.ReadLine());
-
-
-            if (añoActual == 0.0f || añoActual == 0.4f || añoActual == 0.6f || añoActual == 1.0f)
+            añoAnterior = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese calificacion del año actual");
+            añoActual = Convert.ToDouble(Console.ReadLine());
+            promedio = (añoActual + añoAnterior) / 2;
+            
+            
+            switch (añoAnterior)
             {
-                if (añoAnterior == 0.0f || añoAnterior == 0.4f || añoAnterior == 0.6f || añoAnterior == 1.0f)
-                {
-                    float promedio = 0.0f;
-             
-
-                    promedio = añoActual + añoAnterior / 2;
-                    Console.WriteLine(promedio);
-
-                    
-                }
-                else
-                {
+                case 0.0:
+                case 0.4:
+                case 0.6:
+                case 1.0:
+                    {
+                        switch (añoActual)
+                        {
+                            case 0.0:
+                            case 0.4:
+                            case 0.6:
+                            case 1.0:
+                                {
+                                    Console.WriteLine(nombre);
+                                    Console.WriteLine(promedio);
+                                    
+                                    switch (añosTrabajo)
+                                    {
+                                        case 0:
+                                            {
+                                                valorAumento = sueldoBase * 0; //0%
+                                                Console.WriteLine("0%");
+                                                Console.WriteLine(valorAumento);
+                                            }
+                                            break;
+                                        case 1:
+                                        case 2:
+                                        case 3:
+                                        case 4:
+                                        case 5:
+                                            {
+                                                if ( 0.0 <= promedio && promedio < 0.4)
+                                                {
+                                                    valorAumento = sueldoBase * 0.5; //5%
+                                                    Console.WriteLine("5%");
+                                                    Console.WriteLine(valorAumento);
+                                                }
+                                                else if (0.4 <= promedio && promedio < 0.6)
+                                                {
+                                                    valorAumento = sueldoBase * 0.1;//10%
+                                                    Console.WriteLine("10%");
+                                                    Console.WriteLine(valorAumento);
+                                                }
+                                            }
+                                            break;
+                                        default:
+                                            valorAumento = sueldoBase * 0.3; //30%
+                                            Console.WriteLine("30%");
+                                            Console.WriteLine(valorAumento);
+                                            break;
+                                            
+                                    
+                        
+                                    }
+                                    break;
+                                }
+                            default:
+                                Console.WriteLine("Error: calificación inválida. El programa terminará.");
+                                break;
+                        }
+                        break;
+                    }
+                default:
                     Console.WriteLine("Error: calificación inválida. El programa terminará.");
-                }
-                
-
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Error: calificación inválida. El programa terminará.");
-            }
-
+        
         }
     }
 }
