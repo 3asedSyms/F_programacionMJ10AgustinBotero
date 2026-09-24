4fe3cdca0943cf4ca441c5910470c96d681614d1
3asedSyms <agus.b1215@protonmail.com>
2026-09-24 02:25:23 -0500

    taller_vectores *incompleto*

--- TallerVectores/TallerVectores/Program.cs
+++ TallerVectores/TallerVectores/Program.cs
@@ -6,6 +6,21 @@ namespace TallerVectores
         static void Main(string[] args)
         {
             // Escribir un algoritmo que permita llenar un vector[15] con números enteros, y luego encuentre y muestre el valor máximo y mínimo de los números ingresados.
+            
+            int[] vector = new int[15];
+            int maximo;
+            int minimo;
+            int record;
+
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                
+                Console.WriteLine($"Introduce el numero {i+1}:");
+                
+                vector[i] = Convert.ToInt16(Console.ReadLine());
+                record = vector[i];
+            }
         }
     }
 }
